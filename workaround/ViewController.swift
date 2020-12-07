5c4275915742e5c12a6eeb300131d9e7d874c370
Vignesh <vignesh.krish1989@gmail.com>
2020-12-07 12:15:10 +0530

    Initial Commit

---
+++ workaround/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  workaround
+//
+//  Created by Vignesh Krishnamurthy on 07/12/20.
+//  Copyright © 2020 FinnovationZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
